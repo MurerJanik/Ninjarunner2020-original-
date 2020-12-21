04ae482e275cee6bf5545bed8b9d0570d862184d
Janik Murer <janik.murer@gmx.ch>
2020-12-21 17:29:30 +0100

    many Changes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    private Rigidbody2D myRigidbody;
+
+    private static Player instance;
+
+    public static Player Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = GameObject.FindObjectOfType<Player>();
+            }
+
+            return instance;
+        }
+    }
 
     private Animator myAnimator;
 
     [SerializeField]
     private float movementSpeed;
-
-    private bool attack;
-
-    private bool slide;
 
     private bool facingRight;
 
@@ -27,168 +37,135 @@ public class Player : MonoBehaviour
     [SerializeField]
     private LayerMask whatIsGround;
 
-    private bool isGrounded;
-
-    private bool jump;
-
-    private bool jumpAttack;
-
     [SerializeField]
     private bool airControl;
 
     [SerializeField]
     private float jumpForce;
 
+    public Rigidbody2D MyRigidbody { get; set; }
+
+    public bool Attack { get; set; }
+
+    public bool Slide { get; set; }
+
+    public bool Jump { get; set; }
+
+    public bool OnGround { get; set; }
+
 
     // Start is called before the first frame update
     void Start()
     {
         facingRight = true;
-        myRigidbody = GetComponent<Rigidbody2D>();
+        MyRigidbody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
     }
 
-    private void Update()
+    void Update()
     {
         HandleInput();
     }
 
     // Update is called once per frame
-    void  FixedUpdate()
+    void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
 
-        isGrounded = IsGrounded();
+        OnGround = IsGrounded();
 
         HandleMovement(horizontal);
 
         Flip(horizontal);
 
-        HandleAttacks();
-
         HandleLayers();
 
-        ResetValues();
     }
 
     private void HandleMovement(float horizontal)
     {
-
-        if (myRigidbody.velocity.y < 0)
+        if (MyRigidbody.velocity.y < 0)
         {
             myAnimator.SetBool("land", true);
         }
-
-        if (!myAnimator.GetBool("slide") && !this.myAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Attack")&& (isGrounded || airControl))
+        if (!Attack && !Slide || OnGround || airControl)
         {
-            myRigidbody.velocity = new Vector2(horizontal * movementSpeed, myRigidbody.velocity.y);
+            MyRigidbody.velocity = new Vector2(horizontal * movementSpeed, myAnimator.velocity.y);
+        }
+        if (Jump && MyRigidbody.velocity.y == 0)
+        {
+            MyRigidbody.AddForce(new Vector2(0, jumpForce));
         }
 
-        if (isGrounded && jump)
-        {
-            isGrounded = false;
-            myRigidbody.AddForce(new Vector2(0, jumpForce));
-            myAnimator.SetTrigger("jump");
-        }
-
-        if (slide && !this.myAnimator.GetCurrentAnimatorStateInfo(0).IsName("Slide"))
-        {
-            myAnimator.SetBool("slide", true);
-        }
-        else if (!this.myAnimator.GetCurrentAnimatorStateInfo(0).IsName("Slide"))
-        {
-            myAnimator.SetBool("slide", false);
-        }
-
-
-        myAnimator.SetFloat("speed", Mathf.Abs( horizontal));
+        myAnimator.SetFloat("speed", Mathf.Abs(horizontal));
     }
 
-    private void HandleAttacks()
-    {
-        if (attack && isGrounded &&!this.myAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Attack"))
+        private void HandleInput()
         {
-            myAnimator.SetTrigger("attack");
-            myRigidbody.velocity = Vector2.zero;
-        }
-
-        if (jumpAttack && !isGrounded && !this.myAnimator.GetCurrentAnimatorStateInfo(1).IsName("JumpAttack"))
-        {
-
-        }
-    }
-
-    private void HandleInput()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            jump = true;
-        }
-
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            attack = true;
-            jumpAttack = true;
-        }
-
-        if (Input.GetKeyDown(KeyCode.LeftControl))
-        {
-            slide = true;
-        }
-    }
-
-    private void Flip(float horizontal)
-    {
-        if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
-        {
-            facingRight = !facingRight;
-
-            Vector3 theScale = transform.localScale;
-
-            theScale.x *= -1;
-
-            transform.localScale = theScale;
-        }
-    }
-
-    private void ResetValues()
-    {
-        attack = false;
-        slide = false;
-        jump = false;
-        jumpAttack = false;
-    }
-
-    private bool IsGrounded()
-    {
-        if (myRigidbody.velocity.y <= 0)
-        {
-            foreach (Transform point in groundPoints)
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                Collider2D[] colliders = Physics2D.OverlapCircleAll(point.position, groundRadius, whatIsGround);
+            myAnimator.SetTrigger("jump");
+            }
+            
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+            myAnimator.SetTrigger("attack");
+            }
 
-                for (int i = 0; i < colliders.Length; i++)
+            if (Input.GetKeyDown(KeyCode.LeftControl))
+            {
+            myAnimator.SetTrigger("Slide");
+            }
+
+        }
+
+        private void Flip(float horizontal)
+        {
+            if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
+            {
+                facingRight = !facingRight;
+
+                Vector3 theScale = transform.localScale;
+
+                theScale.x *= -1;
+
+                transform.localScale = theScale;
+            }
+        }
+
+
+
+        private bool IsGrounded()
+        {
+            if (MyRigidbody.velocity.y <= 0)
+            {
+                foreach (Transform point in groundPoints)
                 {
-                    if (colliders[i].gameObject != gameObject)
+                    Collider2D[] colliders = Physics2D.OverlapCircleAll(point.position, groundRadius, whatIsGround);
+
+                    for (int i = 0; i < colliders.Length; i++)
                     {
-                        myAnimator.ResetTrigger("jump");
-                        myAnimator.SetBool("land", false);
-                        return true;
+                        if (colliders[i].gameObject != gameObject)
+                        {
+                            return true;
+                        }
                     }
                 }
             }
+            return false;
         }
-        return false;
-    }
-    private void HandleLayers()
-    {
-        if (!isGrounded)
+        private void HandleLayers()
         {
-            myAnimator.SetLayerWeight(1, 1);
+            if (!OnGround)
+            {
+                myAnimator.SetLayerWeight(1, 1);
+            }
+            else
+            {
+                myAnimator.SetLayerWeight(1, 0);
+            }
         }
-        else
-        {
-            myAnimator.SetLayerWeight(1, 0);
-        }
-    }
+    
 }
+ 
+
